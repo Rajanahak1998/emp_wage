3acb8f654c0b8898418fe026af429f0c5f6faf3a
susant <rajanahak1998@gmail.com>
2021-06-02 22:51:09 +0530

    emp wage added

--- emp_wage.sh
+++ emp_wage.sh
@@ -4,3 +4,14 @@ totalSalary=0
 empRatePerHr=20
 numWorkingDays=0
 hrcheck=0
+empCheck=$((RANDOM%2))
+if [ $empCheck -eq $present ]
+then
+	echo "Employee present"
+	workingHr=8
+else
+	echo "Employee absent"
+	workingHr=0
+fi
+salary=$((wagePerHour*workingHr))
+echo "Daily employee wage : $salary"
